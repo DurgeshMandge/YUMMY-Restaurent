0931d17dd13a4e0b08d7c4a8d82cdb3d1b8f15a6
Durgesh Mandge <117802796+DurgeshMandge@users.noreply.github.com>
2023-06-14 09:26:34 +0530

    Documented

--- components/Body.js
+++ components/Body.js
@@ -13,16 +13,27 @@ const Card = ({cloudinaryImageId,name,cuisines,city}) =>{
     );
 }
 
-
-    
+        //1. Hooks
+        //2. Onchange Functionality (update searchText -> e.target.value)
+        //3. Onclick functionality  (update restList -> filter data)
+        //4. Mapping Functionality
+        //5. Filtering Functionality
+        
 const Body = () =>{
+
+
+    //---->     Filtering Functionality
 
     function filterData(Restaurent,searchText){
         return (Restaurent.filter((rest)=>rest.data.name.includes(searchText)));
     }
 
+
+    //---->     Hooks
+
     const [searchText,setSearchText]=useState("");
     const [Restaurants,setRestList]=useState(RestList);
+
     return (
     <>
     <div className="body">
@@ -30,11 +41,17 @@ const Body = () =>{
             type="text"
             placeholder="search"
             value={searchText}
+
+    //---->   Onchange Functionality
             onChange={(e)=>{
                 setSearchText(e.target.value);
             }}
         />
+
         <button type="submit"
+
+    //---->      Onclick functionality
+
             onClick={
                 ()=>{setRestList(filterData(Restaurants,searchText));
             }}
@@ -42,9 +59,13 @@ const Body = () =>{
     </div>
 
     <div className="cardList">
+
+{/*  //----> Mapping Functionality                                  */}
+
         {Restaurants.map((rest)=>{
             return (<Card {...rest.data} {...rest.data.slugs} key={rest.data.id}/>);
         })}
+
     </div>
     
     </>
